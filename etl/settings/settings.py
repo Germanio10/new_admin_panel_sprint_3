91a0eb04f92d7bd3aaa7e414acf97bee3b787d3d
German Chaley <chaleyg2@gmail.com>
2024-01-11 20:02:02 +0300

    add getenv

--- etl/settings/settings.py
+++ etl/settings/settings.py
@@ -1,11 +1,16 @@
+import os
 from elasticsearch import Elasticsearch
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 es = Elasticsearch(hosts='http://127.0.0.1:9200/')
 
 dsl = {
-    'dbname': 'movies_database',
-    'user': 'app',
-    'password': '123qwe',
-    'host': '127.0.0.1',
-    'port': 5432
+    'dbname': os.getenv('DB_NAME'),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD'),
+    'host': os.getenv('DB_HOST'),
+    'port': os.getenv('DB_PORT')
 }
